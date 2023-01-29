2a48396b4d1df8f8da701e8b93d126405dbbedfc
Victor Akintunde <akintoluvic@gmail.com>
2023-01-29 17:25:07 +0100

    search

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Lyric from "./Components/Lyric";
+import { ContextP } from "./Components/Context";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContextP>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/lyric/track/:id" element={<Lyric />} />
+          </Routes>
+        </div>
+      </Router>
+    </ContextP>
   );
 }
-
 export default App;
---
+++ src/Components/Context.tsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect, createContext } from "react";
+import axios from "axios";
+
+interface ContextPro {
+  track_list?: ({} | null)[] | string[] | number;
+  heading?: ({} | null)[] | [] | " ";
+  [key: string]: any;
+}
+
+// export const Context = createContext<ContextPro | null>(null);
+export const Context = createContext({} as ContextPro);
+//export const Context = createContext<ContextPro | undefined>(undefined);
+
+export const ContextP: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const [state, setState] = useState<ContextPro[] | null | {} | string[]>([
+    {
+      track_list: [],
+      heading: " ",
+    },
+  ]);
+
+  useEffect(() => {
+    axios
+      .get(
+        ` http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
+      )
+      .then((res) => {
+        console.log(res.data);
+        setState({
+          track_list: res.data.message.body.track_list,
+          heading: "Top 10 Tracks",
+        });
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <Context.Provider value={[state, setState]}>{children}</Context.Provider>
+  );
+};
