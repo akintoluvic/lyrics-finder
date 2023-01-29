79870bbd0cb17138a68e5bc3fa03a8bc223183d6
Victor Akintunde <akintoluvic@gmail.com>
2023-01-29 17:54:37 +0100

    unused line

--- src/Components/Lyric.tsx
+++ src/Components/Lyric.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
-//import Moment from "react-moment";
 
 const Lyric = () => {
   const [track, setTrack] = useState<any>([]);
