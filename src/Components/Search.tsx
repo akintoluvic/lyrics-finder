02e53f2a1bc6f5549e3e62cb3e5572bdf25af8ef
Victor Akintunde <akintoluvic@gmail.com>
2023-02-08 12:19:24 +0100

    add dependency

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -10,14 +10,14 @@ const Search = () => {
   useEffect(() => {
     axios
       .get(
-        `https://proxy.cors.sh/http://api.musixmatch.com/ws/1.1/track.search?q_track=${trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
+        `http://api.musixmatch.com/ws/1.1/track.search?q_track=${trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
       )
       .then((res) => {
         let track_list = res.data.message.body.track_list;
         setState({ track_list: track_list, heading: "Search Results" });
       })
       .catch((err) => console.log(err));
-  }, [trackTitle]);
+  }, [trackTitle, setState]);
   const findTrack = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setTrackTitle(userInput);
